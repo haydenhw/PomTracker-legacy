058d20b8d2c44ff009ee589ccec192760f3f843f
hayden7913 <hayden321@gmail.com>
2016-12-13 17:45:27 -0800

    make it better

---
+++ public/app.js
@@ -0,0 +1,227 @@
+
+
+
+
+const state = {
+	projects: []
+}
+
+const toHours = (min) => {
+	return Number((Number(min)/60).toFixed(2));
+}
+
+console.log(typeof toHours(90));
+
+const deleteProj = (state, idx) => {
+	$.ajax({
+			url: "/delete",
+			type: "DELETE",
+			data: state.projects[idx]
+		});
+	state.projects.splice(idx, 1);
+}
+
+const renderProject = (state, elems, name, total, parent, idx) => { 
+ let project = state.projects[idx];
+ let template = $( 
+	`<div id="wrapper">
+		<div class="timeMod well">
+				<div class="topRow">
+					<span class="title">${name}</span>
+					<span class="acctotal">${total.toFixed(2)}</span>
+					<div class="form-group sell">
+						<label for="sell"></label>
+						<select name="" id="sell" class="form-control">
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+						</select>
+					</div>
+				</div>
+			<div class="btn-group timeButtons">
+				<button type="button" class="js-btn5 btn btn-primary">5</button>
+				<button type="button" class="js-btn15 btn btn-primary">15</button>
+				<button type="button" class="js-btn25 btn btn-primary" value="25">25</button>
+				<input type="text" name="" id="customInput" class="customInput form-control">
+			</div>
+			<div class="controlButtons">
+				<button type="button" id="js-reset" class="btn btn-primary" >Reset</button>
+				<button type="button" id="js-undo" class="btn btn-primary" >Undo</button>
+				<button type="button" id="js-delete" class="btn btn-primary" >Delete</button>
+			</div>
+		</div>
+	</div>`)
+
+
+ 	template.find(".js-btn5").click( () => { 
+ 		project.addTime(5);
+ 		console.log("add time 5" , moment().format("h:mm:ss"));
+ 	});
+
+ 	template.find(".js-btn15").click( () => { 
+		project.addTime(15);
+ 		console.log("add time 15" , moment().format("h:mm:ss"));
+
+
+ 	});
+
+ 	template.find(".js-btn25").click( () => { 
+ 		project.addTime(25);
+ 		console.log("add time 25" , moment().format("h:mm:ss"));
+
+
+ 	});
+
+ 	template.find("#js-reset").click( () => {
+ 		project.reset();
+ 	});
+
+ 	template.find("#customInput").on("keyup", (e) => {
+ 		const code = e.which;
+ 		if (code == 13) {
+ 			e.preventDefault();
+ 			project.addTime(Number($("#customInput").val()));
+ 			console.log((new Date()).toDateString());
+ 			renderList(state, elems);
+ 			updateAll(state);
+ 			//this.reset;
+ 		}
+ 	})
+
+ 	template.find("#js-undo").click( () => {
+ 		project.undo();
+ 		renderList(state, elems);
+ 	})
+
+ 	template.find("#js-delete").click( () => {
+ 		deleteProj(state, idx);
+ 	});
+
+ 	template.find(".btn").click(() => {
+ 		renderList(state, elems);
+ 		updateAll(state);
+
+ 	});
+ 	return template;
+}
+
+const renderList = (state, elems) => {
+	let resHtml = state.projects.map((proj, idx) => {
+
+		return renderProject(state, elems, proj.name, proj.total, proj.parent, idx) 
+	});
+
+	elems.projectList.html(resHtml);
+}
+
+function Parent (name, total) {
+ 	this.name = name; 
+ 	this.total = Number(total);
+
+}
+
+Parent.prototype.addTime = function(t) {
+
+	this.total += toHours(t);
+}
+
+function Proj(name, total, hist, parent) {
+	this.name = name;
+	this.parent = parent;
+	this.total = Number(total);
+	this.histCount = 0;
+	if (hist) {
+		this.hist = history;		
+	} else {
+		this.hist = [];
+	}
+
+}
+
+Proj.prototype.parentAddTime = function() {
+
+}
+
+Proj.prototype.addTime = function(t) {
+	console.log(toHours(t));
+	this.total += toHours(t);
+	if (this.hist) {
+		 this.hist.push(this.total);
+		 this.histCount = 0;
+	}
+
+
+}	
+
+Proj.prototype.reset = function() {
+	this.total = 0;
+}
+
+Proj.prototype.undo = function() {
+		if (this.hist) {
+			if (this.hist.length > 1 && this.hist.length-this.histCount-2 >= 0) {
+				this.total = this.hist[this.hist.length-this.histCount-2]; 
+				this.histCount++;
+			}
+
+			if (this.hist.length > 20) {
+				this.hist.splice(0,1);
+			}
+		}
+}
+
+const initSubmitHandler = (state, elems) => {	
+	$(elems.newProj).on("submit", (e) => {
+		e.preventDefault();
+		let name;
+		name = $("#projName").val();
+		state.projects.push(new Proj(name, 0));
+		console.log();
+		saveProject(state.projects[state.projects.length-1]);
+		renderList(state, elems);
+	}); 
+}
+const saveProject = (project) => {
+	console.log(project);
+	const callback = () => console.log("Project Saved");
+
+	$.post("/save", project, callback, "json");
+}
+
+const updateAll = (state) => {
+	state.projects.forEach((proj) => {
+		$.ajax({
+			url: "/update",
+			type: "PUT",
+			data: proj, 
+			success: (data) => {console.log("save successful")}
+		});
+	});
+}
+
+const setState = (state, elems) => {
+	const callback = (data) => {
+		/*state.projects = data; 
+		console.log(state.projects);*/
+		
+		state.projects = data.map( project => {
+		return new Proj(project.name, project.total, project.hist, project.parent)
+		});
+		renderList(state, elems);
+	}
+	$.get("/getData", undefined, callback);
+}
+const main = () => {
+	const elems = {
+		newProj : $("#newProj"),
+		projectList: $("#projectList")
+	}
+	setState(state, elems);
+	initSubmitHandler(state, elems)
+
+}
+
+$(main); 
+
+
+
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const bp = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+
+
+app.use(bp.json());
+app.use(bp.urlencoded({extended: true}));
+app.use(express.static('public'))
+
+let db, server;
+
+app.get('/getData', (req, res) => {
+	db.collection('projects').find().toArray(
+		function(err, results){
+			res.send(results);
+		});
+});
+
+app.post('/save', (req, res) => {
+	console.log(req.body);
+	db.collection('projects').save(req.body, (err, result) => {
+		if (err) return console.log(err);
+		console.log('saved to database');
+		res.redirect('/')
+	});
+});
+
+app.put('/update', (req, res) => {
+	db.collection('projects')
+	.findOneAndUpdate({name : req.body.name}, {
+		$set: {
+			name: req.body.name,
+			parent: req.body.parent,
+			total: req.body.total
+		} 
+	}, (err, result) => {
+		if(err) console.log(err);
+		res.send(result);
+		//console.log('Save Successful');
+	});
+});
+
+app.delete('/delete', (req,res) => {
+	console.log(req.body.name);
+	console.log(req.body);
+	db.collection('projects').findOneAndDelete({name : req.body.name},
+	 (err, result) => {
+		console.log(result, 'deleted');
+		db.collection('projects').find().toArray(
+		function(err, results){
+			//console.log(results);
+		});
+	});
+});
+
+MongoClient.connect('mongodb://hayden321:46869269a@ds127878.mlab.com:27878/pomtimer', (err, database) => {
+	if (err) return console.log(err);
+	db = database; 
+	server = app.listen(process.env.PORT || 3000, () => {
+		console.log("Listening");
+	});
+	
+});
