6a2ec7e233f9ba660380b52a01abb40c332f633c
hayden7913 <hayden321@gmail.com>
2017-02-01 10:56:06 -0800

    switching over

--- server.js
+++ server.js
@@ -1,136 +1,65 @@
 const express = require('express');
 const app = express();
 const bp = require('body-parser');
-const mongoose = require('mongoose');
-const faker = require('faker');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
-mongoose.Promise = global.Promise;
-
-const {PORT, DATABASE_URL} = require('./config');
-const {PomTracker} = require('./models');
 
 app.use(bp.json());
-app.use(express.static('public'));
-app.use(bp.urlencoded({
-  extended: true
-}));
+app.use(bp.urlencoded({extended: true}));
+app.use(express.static('public'))
 
-app.get('/tasks', (req, res) => {
-  PomTracker
-    .find()
-    .exec()
-    .then(tasks => {
-      res.json({
-        tasks: tasks.map(
-          task => task.apiRepr()
-        )
-      });
-    })
-    .catch(
-      err => {
-        console.error(err);
-        res.status(500).json({message: 'Internal Server Error'});
-      }
-    )
-});
+let db, server;
 
-
-app.post('/tasks', (req,res) => {
-console.log(req.body);
-	PomTracker
-		.create({
-			"name": req.body.name,
-			"parent": req.body.parent,
-			"total": req.body.total
-		})
-		.then(
-			task => res.status(201).json(task.apiRepr()))
-		.catch(err => {
-				console.error(err);
-				res.status(500).json({message: 'Internal server error'});
+app.get('/getData', (req, res) => {
+	db.collection('projects').find().toArray(
+		function(err, results){
+			res.send(results);
 		});
 });
 
-app.put('/tasks/:id', (req, res) => {
-  toUpdate = {"total": req.body.total}
-  PomTracker
-    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
-    .exec()
-    .then(task => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'Interval server error'}));
+app.post('/save', (req, res) => {
+	console.log(req.body);
+	db.collection('projects').save(req.body, (err, result) => {
+		if (err) return console.log(err);
+		console.log('saved to database');
+		res.redirect('/')
+	});
 });
 
-app.delete('/tasks/:id', (req, res) => {
-  PomTracker
-    .findByIdAndRemove(req.params.id)
-    .exec()
-    .then(task => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
-})
+app.put('/update', (req, res) => {
+	db.collection('projects')
+	.findOneAndUpdate({name : req.body.name}, {
+		$set: {
+			name: req.body.name,
+			parent: req.body.parent,
+			total: req.body.total
+		}
+	}, (err, result) => {
+		if(err) console.log(err);
+		res.send(result);
+		//console.log('Save Successful');
+	});
+});
 
+app.delete('/delete', (req,res) => {
+	console.log(req.body.name);
+	console.log(req.body);
+	db.collection('projects').findOneAndDelete({name : req.body.name},
+	 (err, result) => {
+		console.log(result, 'deleted');
+		db.collection('projects').find().toArray(
+		function(err, results){
+			//console.log(results);
+		});
+	});
+});
 
-//////Generate Data
-const generateParent = () => {
-  const parents = ["Node Capstone", "Goals", "Chores"];
-  return parents[Math.floor(Math.random() * parents.length)];
-}
+MongoClient.connect('mongodb://hayden321:46869269a@ds127878.mlab.com:27878/pomtimer', (err, database) => {
+	if (err) return console.log(err);
+	db = database;
+	server = app.listen(process.env.PORT || 3000, () => {
+		console.log("Listening on port 3000");
+	});
 
-const generateTaskData = () => {
-  return {
-    name: faker.lorem.word(),
-    parent: generateParent(),
-    total: Math.floor(Math.random()*20)
-  }
-}
-
-const seedTaskData = () => {
-  const seedData = [];
-
-  for (let i = 0; i < 10; i++) {
-    seedData.push(generateTaskData());
-  }
-  return PomTracker.insertMany(seedData);
-}
-
-//seedTaskData();
-
-let server;
-
-function runServer(databaseUrl=DATABASE_URL, port=PORT) {
-  return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
-      .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
-      });
-    });
-  });
-}
-
-
-function closeServer() {
-  return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
-  });
-}
-
-if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
-
-module.exports = {app, runServer, closeServer};
+});
