cbfd8e2a2b67d301f336b7f6b621607e6431080a
hayden7913 <hayden321@gmail.com>
2017-02-02 11:03:08 -0800

    added project contructor

--- public/app.js
+++ public/app.js
@@ -1,54 +1,225 @@
+testData = {
+  "projects": [
+    {
+      "_id": "5892bdffb228b27570d7c3be",
+      "__v": 0,
+      "projectName": "quidem",
+      "tasks": [
+        {
+          "taskName": "qui",
+          "total": 16,
+          "_id": "5892bdffb228b27570d7c3c3",
+          "log": [
+            {
+              "startTime": "20:51",
+              "endTime": "17:03",
+              "_id": "5892bdffb228b27570d7c3c4"
+            }
+          ]
+        },
+        {
+          "taskName": "voluptatem",
+          "total": 9,
+          "_id": "5892bdffb228b27570d7c3c1",
+          "log": [
+            {
+              "startTime": "6:20",
+              "endTime": "13:54",
+              "_id": "5892bdffb228b27570d7c3c2"
+            }
+          ]
+        },
+        {
+          "taskName": "minus",
+          "total": 12,
+          "_id": "5892bdffb228b27570d7c3bf",
+          "log": [
+            {
+              "startTime": "18:46",
+              "endTime": "7:27",
+              "_id": "5892bdffb228b27570d7c3c0"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "_id": "5892bdffb228b27570d7c3c5",
+      "__v": 0,
+      "projectName": "est",
+      "tasks": [
+        {
+          "taskName": "consequatur",
+          "total": 5,
+          "_id": "5892bdffb228b27570d7c3c8",
+          "log": [
+            {
+              "startTime": "18:27",
+              "endTime": "6:11",
+              "_id": "5892bdffb228b27570d7c3c9"
+            }
+          ]
+        },
+        {
+          "taskName": "iure",
+          "total": 16,
+          "_id": "5892bdffb228b27570d7c3c6",
+          "log": [
+            {
+              "startTime": "3:05",
+              "endTime": "9:24",
+              "_id": "5892bdffb228b27570d7c3c7"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
 
 
 const state = {
-	projects: ["none"],
-	tasks: []
+	projects: ["none", "Sample Project"],
+	tasks: [],
+	errorMessage: {
+		duplicateProject: 'That project already exists. Please use a different project name',
+		duplicateTask: 'That task already exists. Please use a different project name',
+		invalidTime: 'Please enter a time that is greater than 0'
+	}
 }
 
-const startTimer = (t) => {
-	//droid cascade
- const intervalSound = new Audio("./sounds/intervalSound.mp3");
- const endSound = new Audio("./sounds/endSound.mp3");
-
- const alertInterval = (interval) => {
-	 console.log(interval);
-	 for (let i = 0; i < 5; i++) {
-		 intervalSound.play();
-	 }
- }
- const alertEnd = () => endSound.play();
-
- intervalTimer(t, 5, alertEnd, alertInterval)
+const displayErrror = (element, error) => {
+	element.text('error');
 }
 
-const toHours = (min) => {
+const minutesToHours = (min) => {
 	return Number((Number(min)/60).toFixed(2));
 }
 
-const deleteTask = (state, idx) => {
-	$.ajax({
+function Task(name, totalTime, project) {
+	this.name = name;
+	this.project = project;
+	this.totalTime = Number(totalTime);
+	this.histCount = 0;
+	this.hist = [totalTime];
+}
+
+Task.prototype.addTime = function(t) {
+	this.totalTime += minutesToHours(t);
+
+	if (this.hist) {
+		 this.hist.push(this.totalTime);
+		 this.histCount = 0;
+	}
+
+	if(this.projectAddTime) {
+		this.projectAddTime(t);
+	}
+
+	renderProjectList(state, elems);
+}
+
+Task.prototype.reset = function() {
+	this.totalTime = 0;
+
+	if(this.hist) {
+		this.hist.push(0);
+	}
+}
+
+Task.prototype.undo = function() {
+	if (this.hist && this.hist.length > 1) {
+		this.hist.pop();
+		this.totalTime = this.hist[this.hist.length - 1];
+	}
+}
+
+function Project(name , tasks) {
+  this.projectName = name;
+  this.tasks = tasks;
+}
+
+Project.prototype.calculateTotalProjectTime = function () {
+   return this.tasks
+            	.map(task => task.totalTime)
+            	.reduce((a,b) => a+b)
+}
+
+const sampleTask1 = new Task("sample task", 1 , "Sample Project");
+const sampleTask2 = new Task("another sample task" , 2 , "Sample Project");
+
+state.tasks.push(sampleTask1);
+state.tasks.push(sampleTask2);
+
+
+const saveTask = (task) => {
+	const callback = () => console.log("Task Saved");
+	const postObj = {
+		"name": task.name,
+		"project": task.project,
+		"totalTime": task.totalTime
+	}
+	$.post("/tasks", postObj, callback, "json");
+}
+
+const updateAllTasks = (state) => {
+	state.tasks.forEach((task) => {
+		$.ajax({
+			url: "/tasks",
+			type: "PUT",
+			data: task,
+			success: (data) => {console.log("save successful")}
+		});
+	});
+}
+
+const getTasks = (state, elems) => {
+	const callback = (data) => {
+		state.tasks = data.tasks.map( task => {
+
+			if(state.projects.indexOf(task.project) === -1) {
+				state.projects.push(task.project);
+			}
+
+			return new Task(task.name, task.totalTime, task.hist, task.project)
+		});
+
+		renderTaskList(state, elems);
+		renderProjectOptions(state, elems);
+		renderProjectList(state, elems);
+	}
+	$.get("/tasks", undefined, callback);
+}
+
+
+const deleteTask = (state, idx, elems) => {
+	/*$.ajax({
 			url: "/tasks",
 			type: "DELETE",
 			data: state.tasks[idx]
-		});
-	state.tasks.splice(idx, 1);
+		});*/
+		bootbox.confirm(
+			`Are you sure you want to delete \"${name}\"`,
+			 () => {
+				 state.tasks.splice(idx, 1);
+				 renderTaskList(state, elems);
+			 });
 }
 
-const deleteProject = (projectName) => {
-
+const deleteProject = (state, elems, projectName) => {
 	state.tasks.forEach(task => {
 		if (task.project === projectName) {
-			task.project = "";
-			console.log(task);
+			task.project = null;
 		}
 	});
+
 	state.projects = state.projects.filter(project => {
 		return project !== projectName;
 	});
-	console.log(state.projects);
-	renderProjects();
-	renderProjectOptions();
-	updateAll(state);
+
+	renderProjectList(state, elems);
+	renderProjectOptions(state, elems);
+	//updateAllTasks(state);
 }
 
 const getProjectValues = () => {
@@ -56,10 +227,11 @@ const getProjectValues = () => {
 		state.tasks.forEach((task) => {
 			obj[task.project] ? obj[task.project] += task.totalTime : obj[task.project] = task.totalTime;
 		});
+
 		return obj;
 }
 
-const renderOneProject = (projectName, value) => {
+const renderOneProject = (state, elems, projectName, value) => {
 	const resHtml = $(
 		`<div>
 			<div id="projectWrapper">
@@ -71,18 +243,18 @@ const renderOneProject = (projectName, value) => {
 	);
 
 	resHtml.find("#deleteProject").click(() => {
-		deleteProject(projectName);
+		deleteProject(state, elems, projectName);
 	});
 	return resHtml;
 }
 
-const renderProjects = () => {
+const renderProjectList = (state, elems) => {
 	const projects = getProjectValues();
 	const resHtml = Object
 		.keys(projects)
 		.filter(key => key !== "none" && key !== "")
 		.map(key => {
-		return renderOneProject(key, projects[key]);
+		return renderOneProject(state, elems, key, projects[key]);
 	});
 	$("#projects").html(resHtml);
 }
@@ -102,6 +274,8 @@ const renderTask = (state, elems, name, totalTime, project, idx) => {
 				<button type="button" class="js-btn15 btn btn-primary">15</button>
 				<button type="button" class="js-btn25 btn btn-primary" value="25">25</button>
 				<input type="text" name="" id="customInput${idx}" class="customInput form-control">
+				<span id="invalidTimeError"></span>
+
 			</div>
 			<div class="controlButtons">
 				<button type="button" id="js-reset" class="btn btn-primary" >Reset</button>
@@ -114,20 +288,14 @@ const renderTask = (state, elems, name, totalTime, project, idx) => {
 
  	template.find(".js-btn5").click( () => {
  		task.addTime(5);
- 		console.log(`add time 5 to ${name}` , moment().format("h:mm:ss"));
  	});
 
  	template.find(".js-btn15").click( () => {
 		task.addTime(15);
- 		console.log(`add time 15 to ${name}` , moment().format("h:mm:ss"));
-
 
  	});
-
  	template.find(".js-btn25").click( () => {
  		task.addTime(25);
- 		console.log(`add time 25 to ${name}` , moment().format("h:mm:ss"));
-
 
  	});
 
@@ -140,33 +308,35 @@ const renderTask = (state, elems, name, totalTime, project, idx) => {
  		if (code == 13) {
  			e.preventDefault();
 			const input = Number($(`#customInput${idx}`).val());
-			console.log(input);
  			task.addTime(input);
- 			console.log(`add time ${input} to ${name}`, moment().format("h:mm:ss"));
- 			renderList(state, elems);
- 			updateAll(state);
+ 			renderTaskList(state, elems);
+ 			//updateAllTasks(state);
  			//this.reset;
  		}
  	})
 
  	template.find("#js-undo").click( () => {
  		task.undo();
- 		renderList(state, elems);
+ 		renderTaskList(state, elems);
  	})
 
  	template.find("#js-delete").click( () => {
- 		deleteTask(state, idx);
+		deleteTask(state, idx, elems)
+		/*bootbox.confirm(
+			`Are you sure you want to delete \"${name}\"`,
+			 () => 'deleted' //deleteTask(state, idx)
+		);*/
  	});
 
  	template.find(".btn").click(() => {
- 		renderList(state, elems);
- 		updateAll(state);
+ 		renderTaskList(state, elems);
+ 		//updateAllTasks(state);
 
  	});
  	return template;
 }
 
-const renderList = (state, elems) => {
+const renderTaskList = (state, elems) => {
 	let resHtml = state.tasks.map((task, idx) => {
 		return renderTask(state, elems, task.name, task.totalTime, task.project, idx)
 	});
@@ -174,136 +344,61 @@ const renderList = (state, elems) => {
 	elems.taskList.html(resHtml.reverse());
 }
 
-renderProjectOptions = () => {
+renderProjectOptions = (state, elems) => {
 	resHtml = state.projects
 	.filter(project => project !== "")
 	.map((project) => {
 		return `<option value="${project}">${project}</option>`;
 	});
 
-	$("#selectProject").html(resHtml);
+	elems.projectSelect.html(resHtml);
 }
 
-
-
-function Task(name, totalTime, hist, project) {
-	this.name = name;
-	this.project = project;
-	this.totalTime = Number(totalTime);
-	this.histCount = 0;
-	if (hist) {
-		this.hist = history;
-	} else {
-		this.hist = [];
-	}
-}
-
-
-Task.prototype.addTime = function(t) {
-	this.totalTime += toHours(t);
-
-	if (this.hist) {
-		 this.hist.push(this.totalTime);
-		 this.histCount = 0;
-	}
-
-	if(this.projectAddTime)
-  	this.projectAddTime(t);
-
-	startTimer(t);
-	renderProjects()
-}
-
-Task.prototype.reset = function() {
-	this.totalTime = 0;
-}
-
-Task.prototype.undo = function() {
-		if (this.hist) {
-			if (this.hist.length > 1 && this.hist.length-this.histCount-2 >= 0) {
-				this.totalTime = this.hist[this.hist.length-this.histCount-2];
-				this.histCount++;
-			}
-
-			if (this.hist.length > 20) {
-				this.hist.splice(0,1);
-			}
-		}
-}
-
-const initProjectsubmitHandler = (state,elems) => {
+const initProjectSubmitHandler = (state,elems) => {
 	$(elems.newProject).on("submit", (e) => {
 		e.preventDefault();
 		let name;
-		name = $("#projectName").val();
+		name = elems.projectName.val();
 		state.projects.push(name);
 		renderProjectOptions(state, elems);
+		renderProjectList(state, elems);
+		elems.projectName.val("");
 		//saveTask(state.tasks[state.tasks.length-1]);
-		//renderList(state, elems);
+		//renderTaskList(state, elems);
 	});
 }
 
 const initTaskSubmitHandler = (state, elems) => {
 	$(elems.newTask).on("submit", (e) => {
 		e.preventDefault();
-		let name, project;
-		name = $("#taskName").val();
-		project = $("#selectProject :selected").text();
-		state.tasks.push(new Task(name, 0, null, project));
-		saveTask(state.tasks[state.tasks.length-1]);
-		renderList(state, elems);
+
+		const name = elems.taskName.val();
+		const selectedProject = $("#selectProject :selected").text();
+		const taskProject = selectedProject === "none" ? undefined : selectedProject;
+		state.tasks.push(new Task(name, 0, taskProject));
+		//saveTask(state.tasks[state.tasks.length-1]);
+		renderTaskList(state, elems);
+		elems.taskName.val("");
 	});
 }
 
-const saveTask = (task) => {
-	const callback = () => console.log("Task Saved");
-	const postObj = {
-		"name": task.name,
-		"project": task.project,
-		"totalTime": task.totalTime
-	}
-	console.log(postObj);
-	$.post("/tasks", postObj, callback, "json");
-}
 
-const updateAll = (state) => {
-	state.tasks.forEach((task) => {
-		$.ajax({
-			url: "/tasks",
-			type: "PUT",
-			data: task,
-			success: (data) => {console.log("save successful")}
-		});
-	});
-}
-
-const setState = (state, elems) => {
-	const callback = (data) => {
-
-		state.tasks = data.tasks.map( task => {
-
-			if(state.projects.indexOf(task.project) === -1)
-				state.projects.push(task.project);
-
-			return new Task(task.name, task.totalTime, task.hist, task.project)
-		});
-
-		renderList(state, elems);
-		renderProjectOptions(state, elems);
-		renderProjects();
-	}
-	$.get("/tasks", undefined, callback);
-}
 const main = () => {
+
 	const elems = {
 		newProject: $("#newProject"),
-		projectselect: $("#selectProject"),
+		projectSelect: $("#selectProject"),
 		newTask : $("#newTask"),
-		taskList: $("#taskList")
+		taskList: $("#taskList"),
+		projectName: $("#projectName"),
+		taskName:$("#taskName")
 	};
 
-	setState(state, elems);
-	initProjectsubmitHandler(state, elems);
+	//getTasks(state, elems);
+	renderProjectOptions(state, elems);
+	renderProjectList(state, elems);
+	renderTaskList(state, elems);
+	initProjectSubmitHandler(state, elems);
 	initTaskSubmitHandler(state, elems);
 
 }
